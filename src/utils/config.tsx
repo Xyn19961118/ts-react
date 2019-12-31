c1ad6bce3f477740e39ef5cee7febc4bf562d124
martt <179570087@qq.com>
2019-12-31 15:12:04 +0800

    提交

--- src/utils/config.tsx
+++ src/utils/config.tsx
@@ -1,4 +1,4 @@
-export const config: object = {
+export const config = {
   appkey: 'RdshydjBvcYZhMZC',
   baseUrl: 'https://www.ainzdm.com/v3/',
   token: '7c0f892b8bd4078b00d6f6ba88f4658b'
--- src/utils/http.tsx
+++ src/utils/http.tsx
@@ -1,49 +1,58 @@
 import axios from 'axios';
 import { config } from './config';
 
-
+console.log(config)
 // // 创建axios实例
-// var instance = axios.create({
-//   timeout: 1000 * 12
+var instance = axios.create({
+  timeout: 1000 * 12,
+  headers: { 'app-key': config.appkey }
+});
+
+// 设置post请求头
+instance.defaults.headers.post['Content-Type'] = 'application/json';
+
+
+// // 添加请求拦截器
+// axios.interceptors.request.use(function (config) {
+//   // 在发送请求之前做些什么
+
+//   return config;
+// }, function (error) {
+//   // 对请求错误做些什么
+//   return Promise.reject(error);
 // });
 
-// // 设置post请求头
-// instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+// 响应拦截器
+instance.interceptors.response.use(
+  // 请求成功
+  res => res.status === 200 ? Promise.resolve(res) : Promise.reject(res),
+  // 请求失败
+  error => {
+    const {
+      response
+    } = error;
+    if (response) {
+      return Promise.reject(response);
+    }
+
+  });
+
+export default instance;
 
 
-// // 响应拦截器
-// instance.interceptors.response.use(
-//   // 请求成功
-//   res => res.status === 200 ? Promise.resolve(res) : Promise.reject(res),
-//   // 请求失败
-//   error => {
-//     const {
-//       response
-//     } = error;
-//     if (response) {
-//       return Promise.reject(response);
-//     }
 
-//   });
+// class Http {
 
-// export default instance;
+//   public request(url: string, methods: string, params: object) {
+//     const method = methods || "get";
+//     // axios({
+//     //   method,
+//     //   url,
+//     //   data: params
+//     // }).then(res=>{
+//     //   console.log(res)
+//     // });
+//   }
+// }
 
-interface IConfig {
-  url: string,
-  methods: string,
-  params: object
-}
-
-class Http {
-
-  public request<IConfig>(url: string, methods: string, params: object) {
-    const method = methods || "get";
-    // pubilc axios({
-    //   method,
-    //   url,
-    //   data: params
-    // });
-  }
-}
-
-export default Http;
+// export default Http;
